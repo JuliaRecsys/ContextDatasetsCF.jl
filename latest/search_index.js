3b60b208f5962ae4e61f565f889efb6b547a65b0
autodocs <autodocs>
2018-12-11 02:32:21 +0000

    build based on c61d4c3

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#ContextDatasetsCF.jl-1",
+    "page": "Introduction",
+    "title": "ContextDatasetsCF.jl",
+    "category": "section",
+    "text": "Documentation for ContextDatasetsCF.jl"
+},
+
+]}
